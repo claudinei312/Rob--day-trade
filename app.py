f83b7667b24f8b180880b9635ce927955cd5636c
claudinei312 <129322307+claudinei312@users.noreply.github.com>
2025-08-26 09:58:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 # Rota principal
 @app.route("/")
 def home():
-    return "Robô de Day Trade rodando 🚀"
+    return render_template("index.html")
 
-# Rota de teste de sinal (opcional)
+# Rota para sinais do robô (exemplo)
 @app.route("/signal")
 def signal():
-    return "Aqui vão os sinais CALL/PUT"
+    return "CALL/PUT: sinal gerado aqui"
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))  # Render define a porta
     app.run(host="0.0.0.0", port=port)
